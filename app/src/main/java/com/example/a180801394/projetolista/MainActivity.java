7a52863319f52a3835c07fed72151fcc0da19108
manutarouco <manutarouco@gmail.com>
2016-04-30 17:28:42 -0300

    Lista

---
+++ app/src/main/java/com/example/a180801394/projetolista/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.a180801394.projetolista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ArrayList<String> listaPaises;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listaPaises = new ArrayList<String>();
+
+        listaPaises.add("Argentina");
+        listaPaises.add("Brasil");
+        listaPaises.add("USA");
+        listaPaises.add("Paraguai");
+        listaPaises.add("Uriguai");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listaPaises
+        );
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+        Toast.makeText(this, listaPaises.get(pos), Toast.LENGTH_LONG).show();
+    }
+}
